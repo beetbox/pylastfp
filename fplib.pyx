0844f18a3dd9188005a7cfcaa977893138f43f26
Adrian Sampson <adrian@radbox.org>
2010-08-18 09:58:52 -0700

    successfully retrieve fingerprint data

--- fplib.pyx
+++ fplib.pyx
@@ -5,31 +5,17 @@ cdef extern from "FingerprintExtractor.h":
     ctypedef struct FingerprintExtractor "fingerprint::FingerprintExtractor":
         void initForQuery(int freq, int nchannels, int duration)
         void initForFullSubmit(int freq, int nchannels)
-        bool process(short* pPCM, size_t num_samples,
-                     bool end_of_stream)
+        int process(short* pPCM, size_t num_samples,
+                     int end_of_stream)
         StringSizePair getFingerprint()
     FingerprintExtractor *newExtractor \
             "new fingerprint::FingerprintExtractor" ()
     void delExtractor "delete" (FingerprintExtractor *fe)
 
-def testrun():
-    cdef FingerprintExtractor *fe = newExtractor()
-    fe.initForQuery(10, 2, -1)
-    cdef StringSizePair out
-    out = fe.getFingerprint()
-
-    fplen = out.second
-    if fplen == 0:
-        # Fingerprinting failed.
-        return None
-    else:
-        #fixme:
-        # Copy the first fplen bytes from out.second and then return.
-        return 5
-
 def fingerprint(pcmiter, samplerate, channels, duration=-1):
     cdef FingerprintExtractor *fe = newExtractor()
     cdef char *bufcstr
+    cdef StringSizePair outpair
     
     samplesize = sizeof(short)
     fe.initForQuery(samplerate, channels, duration)
@@ -37,16 +23,20 @@ def fingerprint(pcmiter, samplerate, channels, duration=-1):
         bufstr = str(buf)
         bufcstr = bufstr
         bufsize = len(buf)/samplesize
-        print 'processing', repr(buf), bufsize
-        res = fe.process(<short*>bufcstr, bufsize, False)
-        print 'processed'
+        res = fe.process(<short*>bufcstr, bufsize, 0)
         if res:
             # Fingerprint ready.
             break
     else:
         # Extractor never became ready.
         return None
-    # Success.
-    return fe.getFingerprint().second
-    # delExtractor(fe)
-        
+
+    outpair = fe.getFingerprint()
+    if outpair.second:
+        # Success.
+        out = outpair.first[:outpair.second]
+    else:
+        # Failure.
+        out = None
+    delExtractor(fe)
+    return out
--- t.py
+++ t.py
@@ -1,6 +1,8 @@
 import fplib
 import mad
 import sys
+import urllib
+import urllib2
 
 def readf(f):
     while True:
@@ -9,5 +11,32 @@ def readf(f):
             break
         yield out
 
+
+# http://code.activestate.com/recipes/146306/
+def formdata_encode(fields):
+    BOUNDARY = '----------form-data-boundary--_$'
+    out = []
+    for (key, value) in fields:
+        out.append('--' + BOUNDARY)
+        out.append('Content-Disposition: form-data; name="%s"' % key)
+        out.append('')
+        out.append(value)
+    out.append('--' + BOUNDARY + '--')
+    out.append('')
+    body = '\r\n'.join(out)
+    content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
+    return content_type, body
+def formdata_post(url, fields):
+    content_type, data = formdata_encode(fields)
+    req = urllib2.Request(url, data)
+    req.add_header('Content-Type', content_type)
+    return urllib2.urlopen(req).read()
+
 f = mad.MadFile(sys.argv[1])
-print fplib.fingerprint(readf(f), f.samplerate(), 2)
+fpdata = fplib.fingerprint(readf(f), f.samplerate(), 2)
+print urllib.quote(fpdata)
+
+url = 'http://www.last.fm/fingerprint/query/'
+params = {
+    
+}
