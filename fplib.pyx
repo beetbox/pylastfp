e486d8205cbcbd95dd774c5e0fea5b36a32e1f55
Adrian Sampson <adrian@radbox.org>
2010-08-17 18:31:37 -0700

    first stab at processing data from mad

--- fplib.pyx
+++ fplib.pyx
@@ -3,10 +3,10 @@ cdef extern from "FingerprintExtractor.h":
         char *first
         unsigned int second
     ctypedef struct FingerprintExtractor "fingerprint::FingerprintExtractor":
-        void initForQuery(int freq, int nchannels, int duration = -1)
+        void initForQuery(int freq, int nchannels, int duration)
         void initForFullSubmit(int freq, int nchannels)
         bool process(short* pPCM, size_t num_samples,
-                     bool end_of_stream = false)
+                     bool end_of_stream)
         StringSizePair getFingerprint()
     FingerprintExtractor *newExtractor \
             "new fingerprint::FingerprintExtractor" ()
@@ -14,7 +14,7 @@ cdef extern from "FingerprintExtractor.h":
 
 def testrun():
     cdef FingerprintExtractor *fe = newExtractor()
-    fe.initForQuery(10, 2)
+    fe.initForQuery(10, 2, -1)
     cdef StringSizePair out
     out = fe.getFingerprint()
 
@@ -26,3 +26,27 @@ def testrun():
         #fixme:
         # Copy the first fplen bytes from out.second and then return.
         return 5
+
+def fingerprint(pcmiter, samplerate, channels, duration=-1):
+    cdef FingerprintExtractor *fe = newExtractor()
+    cdef char *bufcstr
+    
+    samplesize = sizeof(short)
+    fe.initForQuery(samplerate, channels, duration)
+    for buf in pcmiter:
+        bufstr = str(buf)
+        bufcstr = bufstr
+        bufsize = len(buf)/samplesize
+        print 'processing', repr(buf), bufsize
+        res = fe.process(<short*>bufcstr, bufsize, False)
+        print 'processed'
+        if res:
+            # Fingerprint ready.
+            break
+    else:
+        # Extractor never became ready.
+        return None
+    # Success.
+    return fe.getFingerprint().second
+    # delExtractor(fe)
+        
--- t.py
+++ t.py
@@ -1,4 +1,13 @@
 import fplib
+import mad
+import sys
 
-print "Hello!"
-print fplib.testrun()
+def readf(f):
+    while True:
+        out = f.read(100)
+        if not out:
+            break
+        yield out
+
+f = mad.MadFile(sys.argv[1])
+print fplib.fingerprint(readf(f), f.samplerate(), 2)
