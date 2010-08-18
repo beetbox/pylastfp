f1103b4d1bde59c2cdf33a2600377267c1019083
Adrian Sampson <adrian@radbox.org>
2010-08-18 10:22:51 -0700

    fingerprint and metadata queries

--- t.py
+++ t.py
@@ -3,6 +3,8 @@ import mad
 import sys
 import urllib
 import urllib2
+import os
+import hashlib
 
 def readf(f):
     while True:
@@ -16,7 +18,7 @@ def readf(f):
 def formdata_encode(fields):
     BOUNDARY = '----------form-data-boundary--_$'
     out = []
-    for (key, value) in fields:
+    for (key, value) in fields.items():
         out.append('--' + BOUNDARY)
         out.append('Content-Disposition: form-data; name="%s"' % key)
         out.append('')
@@ -32,11 +34,43 @@ def formdata_post(url, fields):
     req.add_header('Content-Type', content_type)
     return urllib2.urlopen(req).read()
 
-f = mad.MadFile(sys.argv[1])
+path = os.path.abspath(sys.argv[1])
+f = mad.MadFile(path)
 fpdata = fplib.fingerprint(readf(f), f.samplerate(), 2)
-print urllib.quote(fpdata)
+# print urllib.quote(fpdata)
 
 url = 'http://www.last.fm/fingerprint/query/'
 params = {
-    
+    'artist': '',
+    'album': '',
+    'track': '',
+    'duration': f.total_time()/1000,
+    'filename': os.path.basename(path),
+    'username': '',
+    'sha256': hashlib.sha256(path).hexdigest(),
 }
+res = formdata_post('%s?%s' % (url, urllib.urlencode(params)),
+                    {'fpdata': fpdata})
+try:
+    fpid, status = res.split()[:2]
+    fpid = int(fpid)
+except ValueError:
+    print 'unparseable response:', res
+if status == 'NEW':
+    print 'new fingerprint:', fpid
+elif status == 'FOUND':
+    print 'fingerprint found:', fpid
+    
+    # Onward!
+    url = 'http://ws.audioscrobbler.com/2.0/'
+    params = {
+        'method': 'track.getFingerprintMetadata',
+        'fingerprintid': fpid,
+        'api_key': '2dc3914abf35f0d9c92d97d8f8e42b43',
+    }
+    out = urllib.urlopen('%s?%s' % (url, urllib.urlencode(params))).read()
+    print out
+
+else:
+    print 'unknown status:', res
+
